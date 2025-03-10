d5dbac2afb13daf2899556a486094bc36fc27829
PlumJE <lje64257@gmail.com>
2025-03-10 10:01:08 +0900

    Corona 템플릿 코드상 등록 2

--- workmate/src/main/java/com/workmate/app/Workmate.java
+++ workmate/src/main/java/com/workmate/app/Workmate.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(basePackages = "com.yedam.app.**.mapper")
 public class Workmate {
-
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(Workmate.class, args);
 	}
