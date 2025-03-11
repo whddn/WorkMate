e7d7331250bd9f62fa95749791847f3ab97a6a37
PlumJE <lje64257@gmail.com>
2025-03-11 14:19:16 +0900

    템플릿 변경(주의 엄청난 양의 파일들이 추가됨)

--- workmate/src/main/java/com/workmate/app/approval/web/ApprovalController.java
+++ workmate/src/main/java/com/workmate/app/approval/web/ApprovalController.java
@@ -2,12 +2,40 @@ package com.workmate.app.approval.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class ApprovalController {
 	@GetMapping("approval/formChoose")
 	public String formChoose(Model model) {
+		return "approval/formChoose";
+	}
+	
+	@GetMapping("approval/formWrite")
+	public String formWriteGet(Model model) {
+		return "approval/annualLeave";
+	}
+	
+	@PostMapping("approval/formWrite")
+	public String formWritePost(Model model) {
+		return "approval/formChoose";
+	}
+	
+	@GetMapping("approval/formRead")
+	public String formReadGet(Model model) {
+		return "approval/annualLeave";
+	}
+	
+	@PutMapping("approval/formRead")
+	public String formReadPut(Model model) {
+		return "approval/formChoose";
+	}
+	
+	@DeleteMapping("approval/formRead")
+	public String formReadDelete(Model model) {
 		return "approval/formChoose";
 	}
 }
