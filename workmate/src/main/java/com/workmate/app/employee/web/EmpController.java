24c88a9590f0f881226f399562a5667050c33daa
ParkHW97 <phw04107@naver.com>
2025-03-12 09:07:37 +0900

    0312 phw04107

---
+++ workmate/src/main/java/com/workmate/app/employee/web/EmpController.java
@@ -0,0 +1,23 @@
+package com.workmate.app.employee.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmpController {
+	@GetMapping("organ") // 조직도 
+	public String empOrganPage(Model model) {
+		return "employees/empOrgan";
+	}
+	
+	@GetMapping("info") // 상세 
+	public String empInfoPage(Model model) {
+		return "employees/empInfo";
+	}
+	
+	@GetMapping("evalu") // 평가 
+	public String empEvaluPage(Model model) {
+		return "employees/evalumain";
+	}
+}
