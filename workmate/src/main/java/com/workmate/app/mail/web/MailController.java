70bd0fdf54dcc8f51e95e09d8aca29aeaaad0913
pc <pc@email.com>
2025-03-12 13:10:52 +0900

    로그인 메일

---
+++ workmate/src/main/java/com/workmate/app/mail/web/MailController.java
@@ -0,0 +1,12 @@
+package com.workmate.app.mail.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MailController {
+	@GetMapping("mailmain")
+	public String mailmain() {
+		return "mail/mailmain";
+	}
+}
