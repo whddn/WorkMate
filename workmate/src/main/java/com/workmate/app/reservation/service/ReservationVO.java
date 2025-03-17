0ca4f8d57eda6ecb888983bf7961c2f1acac43c0
종우 <ghjkl133120@naver.com>
2025-03-17 17:52:21 +0900

    2025-03-17 feat:관리자 crud완성, 예약기능 수정중

--- workmate/src/main/java/com/workmate/app/reservation/service/ReservationVO.java
+++ workmate/src/main/java/com/workmate/app/reservation/service/ReservationVO.java
@@ -2,18 +2,26 @@ package com.workmate.app.reservation.service;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class ReservationVO {
 	private Integer reserNo;			// 예약번호
 	private String name; 				// 이름
-	private Integer userNo;			// 사원번호
-	private String reserType;			// 예약종류
-	private Date reserDate;			// 예약날짜
+	private Integer userNo;				// 사원번호
+	private String commonKind;			// 공용품종류
+	private Date reserDate;				// 예약날짜
+	@DateTimeFormat(pattern = "HH:mm")	
 	private Date reserStartTime;		// 예약시작시간
-	private Date reserEndTime;		// 예약종료시간
+	@DateTimeFormat(pattern = "HH:mm")
+	private Date reserEndTime;			// 예약종료시간
 	private String reserAvailability;	// 예약가능여부
 	private String content;				// 내용
 	private String commonNo;			// 공용품번호
+	private String image;				// 이미지
+	private String usageStatus;			// 사용여부
+	
+	
 }
