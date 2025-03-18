d77c9a634cfc2f07831715a3092289081be7ca1f
종우 <ghjkl133120@naver.com>
2025-03-18 17:54:28 +0900

    2025-03-18 docs: 관리자 예약 수정중

---
+++ workmate/src/main/java/com/workmate/app/admin/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.workmate.app.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // ✅ /uploads/** 요청이 /home/user/uploads/ 실제 폴더와 매핑되도록 설정
+        registry.addResourceHandler("/http://localhost:8099/workmate/**")
+                .addResourceLocations("file:///C:/CommonItemImage/"); // ✅ 실제 파일 경로
+
+    }
+}
+
