b4f50f562bee177560400ce2f5fab33322725b80
phw04107 <phw04107@naver.com>
2025-03-21 00:58:56 +0900

    0321...

--- workmate/src/main/java/com/workmate/app/finance/mapper/FinanceMapper.java
+++ workmate/src/main/java/com/workmate/app/finance/mapper/FinanceMapper.java
@@ -3,5 +3,5 @@ package com.workmate.app.finance.mapper;
 import com.workmate.app.finance.service.FinanceVO;
 
 public interface FinanceMapper {
-	public String selectReportList(FinanceVO financeVO);
+	public FinanceVO selectReportList(FinanceVO financeVO);
 }
