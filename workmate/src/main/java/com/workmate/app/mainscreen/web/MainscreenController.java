a8ca17c03185adf1cf1b619674bcfb93a0d2df25
PlumJE <lje64257@gmail.com>
2025-03-21 15:53:52 +0900

    검색기능 구현완료(아마도)

--- workmate/src/main/java/com/workmate/app/mainscreen/web/MainscreenController.java
+++ workmate/src/main/java/com/workmate/app/mainscreen/web/MainscreenController.java
@@ -1,18 +1,37 @@
 package com.workmate.app.mainscreen.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.workmate.app.mainscreen.service.MenuService;
+import com.workmate.app.mainscreen.service.MenuVO;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class MainscreenController {
+	private final MenuService menuService;
+	
 	@GetMapping("/")
 	public String base(Model model) {
 		return "mainscreen/main";
 	}
 	
 	@GetMapping("/search")
-	public String search(Model model) {
+	public String getSearch(Model model) {
 		return "mainscreen/search";
+	}
+	
+	@PostMapping("/search/start")
+	@ResponseBody
+	public List<MenuVO> postSearchStart(@RequestBody MenuVO menuVO) {
+		return menuService.findMenuList(menuVO);
 	}
 }
