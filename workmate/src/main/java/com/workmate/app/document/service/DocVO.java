e9c7f64e851c8790fe9ff376e13f990ec9d01743
ziyaaa0 <jhee97e@gmail.com>
2025-03-26 17:44:36 +0900

    25-03-26 근태 완료 / 자료실 다운로드 삭제기능추가

--- workmate/src/main/java/com/workmate/app/document/service/DocVO.java
+++ workmate/src/main/java/com/workmate/app/document/service/DocVO.java
@@ -7,18 +7,24 @@ import lombok.Data;
 @Data
 public class DocVO {
 	
-	private int documentNo; //자료번호	
-	private String fileName; //파일명
-	private String fileDesc; //파일명
-	private Long fileSize; //파일크기
-	private String attachment; //첨부파일
-	private String	extenstion; //확장자
-	private Date fileRegDate; //등록날짜
-	private String	fileTag; //태그
-	private Integer userNo; //사원번호
-	private String TeamNo; //팀번호
-	private String userId; //사원이이디
-	private Integer downUser; //다운로드사원
-	private Integer updateUser; //등록한 사원
+	// 자료실 
+	private int documentNo; 		//자료번호	
+	private String fileName; 		//파일명
+	private String fileDesc; 		//파일명
+	private Long fileSize; 			//파일크기
+	private String formattedSize;	//파일크기포맷변환	
+	private String attachment; 		//첨부파일
+	private String	extenstion; 	//확장자
+	private Date fileRegDate; 		//등록날짜
+	private String	fileTag; 		//태그
+	private Integer userNo; 		//사원번호
+	private String TeamNo; 			//팀번호
+	private String userId; 			//사원이이디	
+	private Integer updateUser; 	//등록한 사원
 	
+	
+	// 다운로드 이력
+	private Integer downUser; 	 	//다운로드사원
+	private Date downloadDate; 		//다운로드 날짜
+	private int downloadNo;  		//다운로드 번호	
 }
