09178f46365755622402162957a8af525e8847ea
PlumJE <lje64257@gmail.com>
2025-03-27 13:13:19 +0900

    2025-03-27 13:23

--- workmate/src/main/java/com/workmate/app/approval/service/ApprovalService.java
+++ workmate/src/main/java/com/workmate/app/approval/service/ApprovalService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 public interface ApprovalService {
 	public List<ApprovalVO> findApprovalList(ApprovalVO approvalVO);
+	public List<ApprovalVO> findApprovalListAboutMe(ApprovalVO approvalVO);
 	public ApprovalVO findApprovalById(ApprovalVO approvalVO);
 	public int inputApproval(ApprovalVO approvalVO);
 	public int modifyApproval(ApprovalVO approvalVO);
