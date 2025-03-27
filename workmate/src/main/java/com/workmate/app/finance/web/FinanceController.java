05b33e3d1e4a04fdf52d7e8414408a0f727bdf8a
phw04107 <phw04107@naver.com>
2025-03-28 00:00:08 +0900

    0328 보 고 서

--- workmate/src/main/java/com/workmate/app/finance/web/FinanceController.java
+++ workmate/src/main/java/com/workmate/app/finance/web/FinanceController.java
@@ -1,16 +1,7 @@
 package com.workmate.app.finance.web;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -18,9 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.workmate.app.finance.service.FinanceService;
 import com.workmate.app.finance.service.ReportVO;
@@ -73,41 +62,13 @@ public class FinanceController {
 	
 	// 입출금 보고서 등록
 	@PostMapping("finance/reportInsert")
-	public ResponseEntity<ReportVO> ReportInsertAjax(ReportVO reportVO) {
+	public ResponseEntity<ReportVO> ReportInsertAjax(@RequestBody ReportVO reportVO) {
 		financeService.inputReportPage(reportVO);
 		return ResponseEntity.ok().build();
 	}
 	
-	// 엑셀 파일 업로드 
-	@PostMapping("finance/fileUpload")
-	@ResponseBody
-	public ResponseEntity<List<Map<String, Object>>> uploadExcel(@RequestParam("file") MultipartFile file) {
-		List<Map<String, Object>> result = new ArrayList<>();
-		
-		try (InputStream is = file.getInputStream();
-				Workbook workbook = new XSSFWorkbook(is)) {
-			
-			Sheet sheet = workbook.getSheetAt(0); // 첫번째 시트
-			
-			for (int i = 1; i <= sheet.getLastRowNum(); i ++) {
-				Row row = sheet.getRow(i);
-				if(row == null) continue;
-				
-				Map<String, Object> rowData = new HashMap<>();
-				//rowData.put("date", getCellValue(row.getCell(0)));
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
 	
-	// 엑셀 셀의 값 받아오기
-	//public String getCellValue(Cell cell) {
 
-	//}
-	
 	
 	// 법인카드 전체 조회 페이지 
 	@GetMapping("finance/corcardList")
