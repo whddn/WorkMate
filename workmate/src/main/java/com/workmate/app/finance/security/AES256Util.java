45308ce7fa44d10bcfedd4618904f2ac3ccd034c
ParkHW97 <phw04107@naver.com>
2025-04-01 10:07:35 +0900

    복호화 오류 수정

--- workmate/src/main/java/com/workmate/app/finance/security/AES256Util.java
+++ workmate/src/main/java/com/workmate/app/finance/security/AES256Util.java
@@ -1,8 +1,10 @@
 package com.workmate.app.finance.security;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.stereotype.Component;
@@ -10,21 +12,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class AES256Util {
 // 법인카드 암호화
-	private final String secretKey = "1234567890123456"; // 16자리 키 
-	private final String transformation = "AES/ECB/PKCS5Padding";
-	
-	public String encrypt(String text) throws Exception {
-	      Cipher cipher = Cipher.getInstance(transformation);
-	        SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(), "AES");
-	        cipher.init(Cipher.ENCRYPT_MODE, key);
-	        return Base64.getEncoder().encodeToString(cipher.doFinal(text.getBytes("UTF-8")));
+	  private static final String key = "1234567890abcdef"; // 16자리 (128bit) 비밀키
+	    private static final String iv = "abcdef1234567890";  // 16자리 IV
+
+	    // 암호화
+	    public static String encrypt(String plainText) throws Exception {
+	        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+	        IvParameterSpec ivSpec = new IvParameterSpec(iv.getBytes());
+	        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+
+	        byte[] encrypted = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+	        return Base64.getEncoder().encodeToString(encrypted);
 	    }
 
-	    public String decrypt(String encryptedText) throws Exception {
-	        Cipher cipher = Cipher.getInstance(transformation);
-	        SecretKeySpec key = new SecretKeySpec(secretKey.getBytes(), "AES");
-	        cipher.init(Cipher.DECRYPT_MODE, key);
-	        return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedText)), "UTF-8");
+	    // 복호화
+	    public static String decrypt(String cipherText) throws Exception {
+	        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+	        IvParameterSpec ivSpec = new IvParameterSpec(iv.getBytes());
+	        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+
+	        byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+	        byte[] decrypted = cipher.doFinal(decodedBytes);
+	        return new String(decrypted, StandardCharsets.UTF_8);
 	    }
 	}
 
